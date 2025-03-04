912c9cf15c7fc8eaadf2bd108c7d55fa431d50e4
herrickfung <herrickfung@gatech.edu>
2025-03-04 17:29:35 -0500

    fix pred_map bug

--- indimap/Maps/pred_map.py
+++ indimap/Maps/pred_map.py
@@ -2,6 +2,7 @@
 contains all functions related to prediction analyses
 
 TDL:
+1. make it capatible with Lasso and Ridge
 2. edit for cross metric predictions
 3. plot graph
 '''
@@ -62,10 +63,12 @@ class PredMap:
 
         source_arr = ['subj', 'inst']
         for source in source_arr:
-            self.pred_from_rand(source)
-            self.pred_from_avg(source)
-            self.pred_from_corr(source)
+            # self.pred_from_rand(source)
+            # self.pred_from_avg(source)
+            # self.pred_from_corr(source)
             self.pred_from_fit(source)
+            exit()
+        print('done')
         
     def compute_corr_map(self):
         """ Load CorrMap object """
@@ -180,14 +183,16 @@ class PredMap:
         X = self.raw_mat[source][:, 0, :, :, :]
         n_bs, n_met, n_subjs, _ = X.shape
         W = np.empty((n_bs, n_met, n_subjs, n_subjs - 1))
+        C = np.empty((n_bs, n_met, n_subjs))
         for bs in range(n_bs):
             for met in range(n_met):
                 for subj in range(n_subjs):
                     other_subjs = [i for i in range(n_subjs) if i != subj]
-                    W[bs, met, subj] = self.train_weights(
-                        X[bs, met, other_subjs, :], 
-                        X[bs, met, subj, :],
-                        model=LinearRegression(),
+                    C[bs, met, subj], W[bs, met, subj] = \
+                        self.train_weights(
+                            X[bs, met, other_subjs, :], 
+                            X[bs, met, subj, :],
+                            model=LinearRegression(),
                         )
 
         # predict on second split
@@ -203,31 +208,46 @@ class PredMap:
                     other_subjs = [i for i in range(n_subjs) if i != subj]
                     x = X[bs, met, other_subjs, :]
                     w = W[bs, met, subj, :]
+                    c = C[bs, met, subj]
                     y = Y[bs, met, subj, :]
-                    y_pred = w @ x
+                    y_pred = self.eval_weights(x, w, c,
+                                               model=LinearRegression(),
+                                               )
                     output[bs, met, subj] = np.corrcoef(y_pred, y)[0, 1]
-        
+
         self.pred_results[source]['fit'] = output
 
     @staticmethod
-    def train_weights(X, Y, model=LinearRegression()):
+    def train_weights(X, Y, model=LinearRegression()) -> np.ndarray:
         """ Do a five-fold cross validation to train weights for each subj """
         n_subjs, n_imgs = X.shape
         stims = np.arange(n_imgs)
 
         k = 5
         kf = KFold(n_splits=k, shuffle=True, random_state=42)
-        subj_weight = np.empty((k, n_subjs))
+        Intercept = np.empty((k, n_subjs))
+        Beta = np.empty((k, n_subjs))
 
         for fold, (train_index, _) in enumerate(kf.split(stims)):
             X_train = X[:, train_index].T
             Y_train = Y[train_index]
             model.fit(X_train, Y_train)
-            subj_weight[fold] = model.coef_
+            Intercept[fold] = model.intercept_
+            Beta[fold] = model.coef_
 
-        subj_weight = np.mean(subj_weight, axis=0)
-        return subj_weight
-        
+        Intercept = np.mean(Intercept)
+        Beta = np.mean(Beta, axis=0)
+        return Intercept, Beta
+
+    @staticmethod
+    def eval_weights(x, w, c, model=LinearRegression()) -> np.ndarray:
+        """ Do evaluation of the predictivity using the fitted weights """
+        model.coef_ = w
+        model.intercept_ = c
+        y_pred = model.predict(x.T)
+        return y_pred
+
+
 
 
 
